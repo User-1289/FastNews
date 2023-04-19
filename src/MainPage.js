3019752b0f64094129bb9af9420ad2a5185309e8
user-1289 <armaanzeyad@gmail.com>
2023-04-19 21:47:44 +0530

    Backgroud made black and got api

--- src/MainPage.js
+++ src/MainPage.js
@@ -7,7 +7,7 @@ export default function Main() {
   {
     let getNewsType = event.target.innerText
    // sessionStorage.setItem("newstype", getNewsType)
-    window.open(`content-news?news=${getNewsType}`);
+    window.open(`content-news?news=${getNewsType}`, "new-Window");
    // alert(event.target.innerText);
   };
 
@@ -17,7 +17,7 @@ export default function Main() {
         <h1 id="news-text">News</h1><br/>
         <h1 onClick={(event) => goToPag(event)} className='news-types'>World News</h1>
         <h1 onClick={(event) => goToPag(event)} className='news-types'>Indian News</h1>
-        <h1 onClick={(event) => goToPag(event)} className='news-types'>Tech News</h1>
+        <h1 onClick={(event) => goToPag(event)} className='news-types'>Technology News</h1>
         <h1 onClick={(event) => goToPag(event)} className='news-types'>Business News</h1>
         <h1 onClick={(event) => goToPag(event)} className='news-types'>Sports News</h1>
         <h1 onClick={(event) => goToPag(event)} className='news-types'>Entertainment News</h1>
--- src/SideNews.js
+++ src/SideNews.js
@@ -4,10 +4,51 @@ import './SideNews.css'
   {
 const urlParams = new URLSearchParams(window.location.search);
 const news = urlParams.get('news');
+let catGory = ''
+
+
+if(news==="Indian News")
+{
+//  alert(searchTerm)
+  fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=eab1631abf374798bc855fffdc90194f`)
+  .then(responce => responce.json())
+  .then(data=>
+  {
+    console.log(data)
+  })
+}
+  else if(news==="World News")
+  {
+      fetch('https://newsapi.org/v2/top-headlines?language=en&apiKey=eab1631abf374798bc855fffdc90194f')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+      else
+      {
+        let arr = news.split(' ')
+        catGory = arr[0]
+        console.log(catGory)
+      }
+
+     // fetch(`https://newsapi.org/v2/top-headlines?category=${catGory}&language=en&apiKey=eab1631abf374798bc855fffdc90194f`)
+     // .then(response => response.json())
+     // .then(data => 
+     // {
+     //   console.log(data);
+     // })
+     // .catch(error => 
+     // {
+     //   console.log(error);
+     // });
+
     return (
-      <div>
+      <div className='side-container'>
         <h1>{news}</h1>
-        {/* rest of the component */}
       </div>
     );
   }
