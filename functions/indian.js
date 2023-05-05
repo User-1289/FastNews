878d14b7c4dacc3f3b13e93e300aa323749585c6
user-1289 <armaanzeyad@gmail.com>
2023-05-05 16:52:11 +0530

    fixing an issue in category feature

--- functions/indian.js
+++ functions/indian.js
@@ -1,7 +1,9 @@
+const axios = require('axios');
+
 exports.handler = async function(event, context) {
   try {
-    const response = await fetch(`https://newsapi.org/v2/everything?q=coding&from=2023-05-03&sortBy=popularity&apiKey=eab1631abf374798bc855fffdc90194f`);
-    const data = await response.json();
+    const response = await axios.get(`https://newsapi.org/v2/everything?q=coding&from=2023-05-03&sortBy=popularity&apiKey=eab1631abf374798bc855fffdc90194f`);
+    const data = response.data;
     return {
       statusCode: 200,
       body: JSON.stringify(data),
