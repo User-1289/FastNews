550ebd8b8502260b86bb4c657556a47d72ec7424
user-1289 <armaanzeyad@gmail.com>
2023-05-05 11:39:56 +0530

    fixing issue in deployment

--- src/Category.js
+++ src/Category.js
@@ -1,45 +1,38 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './category.css';
 
-export default function Category(props) 
-{
-    const [isMobile, setIsMobile] = useState(false);
-    const [windowSize, setWindowSize] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-  const spanRef = useRef(null);//to get the spans text that contains categories
-  const [category, setCategory] = useState('');//to get the input's value
-  //const [container, deleteContainer] = useState(false);//to delete the container
-  const [catArr, setCatArr] = useState([]);//this array contains all the categories user entered
-  const [newsData, setNewsData] = useState([]);//this is used to send data to the app.js to diisplay news there
- // const [spanTxt, setSpanTxt] = useState('');
-
+export default function Category(props) {
+  const [isMobile, setIsMobile] = useState(false);
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const spanRef = useRef(null);
+  const [category, setCategory] = useState('');
+  const [catArr, setCatArr] = useState([]);
+  const [newsData, setNewsData] = useState([]);
 
   useEffect(() => {
-    //alert(windowSize.width)
-      if (windowSize.width <= 768) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
-      }
-}, [])
+    if (windowSize.width <= 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, [windowSize]);
 
   useEffect(() => {
-    setCatArr(JSON.parse(localStorage.getItem("Categories")))
+    setCatArr(JSON.parse(localStorage.getItem('Categories')));
   }, []);
 
-  const setCatVal = (event) => 
-  {
+  const setCatVal = (event) => {
     setCategory(event.target.value);
   };
 
-  const saveCategory = async () => 
-  {
+  const saveCategory = async () => {
     let newArr = [...catArr];
     newArr.push(category);
-     setCatArr(newArr);
-    localStorage.setItem("Categories", JSON.stringify(newArr))
+    setCatArr(newArr);
+    localStorage.setItem('Categories', JSON.stringify(newArr));
     setCategory('');
     await getSelCat(category);
   };
@@ -57,28 +50,19 @@ export default function Category(props)
     }
   }
 
- // const delCategory = () => {
- //   deleteContainer(true);
- // };
-
-  function displayCat(categoryVal) 
-  {
-   // alert(isMobile)
-    if(isMobile===true)
-    {
-    document.getElementById('root').style.opacity = "1"
-        document.querySelector(".sidebar").style.display = "none"
-        document.querySelector(".category-txt").style.display = "none"    
-        document.querySelector(".menu-cl").style.display = "inline"
-        document.querySelector(".cancel-cl").style.display = "none"
+  function displayCat(categoryVal) {
+    if (isMobile === true) {
+      document.getElementById('root').style.opacity = '1';
+      document.querySelector('.sidebar').style.display = 'none';
+      document.querySelector('.category-txt').style.display = 'none';
+      document.querySelector('.menu-cl').style.display = 'inline';
+      document.querySelector('.cancel-cl').style.display = 'none';
     }
     const spanText = spanRef.current.getAttribute('val');
-   // setSpanTxt(spanText);
-   async function takeData()
-   {
-    await getSelCat(categoryVal);
-   }
-   takeData()
+    async function takeData() {
+      await getSelCat(categoryVal);
+    }
+    takeData();
   }
 
   useEffect(() => {
@@ -89,26 +73,24 @@ export default function Category(props)
     <>
       <div className='category-txt'>
         <div className='input-container'>
-        <input id="category-input" onChange={setCatVal} value={category} />
-        <span onClick={saveCategory} className="material-symbols-outlined">
-          check_circle
-        </span>
+          <input id='category-input' onChange={setCatVal} value={category} />
+          <span onClick={saveCategory} className='material-symbols-outlined'>
+            check_circle
+          </span>
         </div>
         <div>
           <br />
           {catArr.map((cat, index) => (
-            <>
-                <div key={index}>
+            <div key={index}>
               <span
                 val={cat}
                 ref={spanRef}
                 onClick={() => displayCat(cat)}
-                className="cat-txt">
+                className='cat-txt'>
                 {cat}
               </span>
-              <span className="material-symbols-outlined">delete</span>
-            </div><br/>
-            </>
+              <span className='material-symbols-outlined'>delete</span>
+            </div>
           ))}
         </div>
       </div>
