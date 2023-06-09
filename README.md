836b035ae23d3184f95b3043a73d1f88b52485f0
user-1289 <armaanzeyad@gmail.com>
2023-06-09 10:25:18 +0530

    fixed a bug in datenews backend

---
+++ README.md
@@ -0,0 +1,10 @@
+# FastNews
+This project is a news website which has a lot of features like personalizing news.   
+Excluding news content that user don't wanna view .
+
+Architecture
+This project uses react as frontend and netlify function as backend
+We use newsapi.org as the news api.
+
+This is also converted into an app 
+https://play.google.com/store/apps/details?id=com.webdevintoapp.news
--- functions/datenews.js
+++ functions/datenews.js
@@ -5,13 +5,7 @@ const mongoose = require("mongoose")
 exports.handler = async function(event, context) {
   let newsKey = JSON.parse(event.body).uniqueKey
 
-  if(newsKey!=process.env.REACT_APP_UNIQUE_KEY)
-  {
-    return{
-      statusCode:500,
-      body:JSON.stringify({message: "You don't have the rights to use this endpoint"})
-    }
-  }
+
   const today = new Date();
 const year = today.getFullYear();
 const month = today.getMonth() + 1
@@ -27,28 +21,25 @@ let checkToday = new Date(todayDate)
       useUnifiedTopology: true,
     });
 
-
-   //await document.save()
-  // let id = '64688334b9782e177893e41d'
-  // await dateCol.findByIdAndUpdate(id, { DateVisited:"2023-5-21"}, { new: true });
    let query = {CategoryName:newsName.toLowerCase()}
-  // let query = {_id:"64688334b9782e177893e41d"}
    let data = await dateCol.find(query).exec()
-   let dbDate = new Date(data[0].DateVisited)
    if(data.length===1)
    {
-    if(dbDate.toISOString()===checkToday.toISOString())
-    {
-      return{
-        statusCode:200,
-        body:JSON.stringify({message:"It have been updated"})
+    let dbDate = new Date(data[0].DateVisited)
+      if(dbDate.toISOString()===checkToday.toISOString())
+      {
+        return{
+          statusCode:200,
+          body:JSON.stringify({message:"It have been updated"})
+        }
       }
-    }
-   const numericalId = data[0]._id.valueOf();
-   await dateCol.findByIdAndUpdate(numericalId, { DateVisited:todayDate}, { new: true });
-console.log(numericalId);
+          else
+          {
+            const numericalId = data[0]._id.valueOf();
+            await dateCol.findByIdAndUpdate(numericalId, { DateVisited:todayDate}, { new: true });
+          }
    }
-   else if(data.length<1)
+   else if(data.length==0)
    {
     let document =  new dateCol({
       DateVisited:todayDate,
